2811c740d73086861b3debd05528a5d598cb8c6b
Nick_PC <sshedl22@gmail.com>
2018-10-19 20:12:47 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+#include <conio.h>
+
+#include "Tree.h"
+using namespace std;
+int main() {
+    int n = 0;
+    Tree Tr('a', 'z', 8);
+    srand(time(nullptr));
+    setlocale(LC_ALL, "Russian");
+    Tr.MakeTree( );
+    if(Tr.exist( )) {
+        Tr.OutTree( );
+        cout << '\n' << "Обход в глубину: ";
+        n = Tr.DFS( );
+        cout << " Пройдено узлов = " << n;
+        cout << '\n' << "Обход в ширину: ";
+        n = Tr.BFS( );
+        cout << " Пройдено узлов = " << n;
+    }
+    else cout << "Дерево пусто!";
+    cout << '\n' << "=== Конец ===";
+
+}
